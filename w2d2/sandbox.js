2bcb87f9f7b1966652b90e2ab6aab5aff84ffdfd
michael-38 <michael.38@outlook.com>
2018-01-23 03:38:19 +0000

    w2d2 sandbox

---
+++ w2d2/sandbox.js
@@ -0,0 +1,20 @@
+// const x = 2;
+// let x;
+
+let x = 0;
+
+function test() {
+  x += 2;
+  console.log(x);
+  ;
+}
+
+
+function test2() {
+  // let x = 3
+  console.log(x)
+}
+
+
+test();
+test2();
